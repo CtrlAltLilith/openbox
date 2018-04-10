486920e4e5342accec0bdfa88a246a3fffdf8370
photon <photon@nailara.net>
2018-04-10 06:11:38 +0200

    committed simple .deb auto-install script..

---
+++ AUTOINSTALL_deb.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+export VERS=3.7-n
+export ARCH=amd64
+
+apt-get update && apt-get -y install gcc \
+          gettext automake autoconf autopoint \
+          libtool libpango1.0-dev pkg-config \
+          libglib2.0-dev libxml2-dev \
+          libstartup-notification0-dev \
+          xorg-dev libimlib2-dev \
+          build-essential devscripts dpkg-dev
+
+./autobuild_deb.sh &&
+     ( dpkg -i ../openbox_${VERS}_$ARCH.deb ||
+         apt-get -y -f install ) &&
+     ln -s -f /usr/bin/openbox-session /etc/alternatives/x-window-manager
---
+++ autobuild_deb.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+./bootstrap && ./configure && ./makedeb debian
